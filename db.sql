fdaf426648a0c25b1a859057def1a419ca24bb4a
benjamin-sagredo <benjamin.sagredo@usach.cl>
2022-06-04 00:41:08 -0400

    Archivos para la pregunta 2

---
+++ db.sql
@@ -0,0 +1,27 @@
+CREATE TABLE paises (
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(256),
+    PRIMARY KEY (id)
+);
+
+INSERT INTO paises(name) VALUES ('Argentina');
+INSERT INTO paises(name) VALUES ('Brasil');
+INSERT INTO paises(name) VALUES ('Bolivia');
+INSERT INTO paises(name) VALUES ('Chile');
+INSERT INTO paises(name) VALUES ('Colombia');
+INSERT INTO paises(name) VALUES ('Costa Rica');
+INSERT INTO paises(name) VALUES ('Cuba');
+INSERT INTO paises(name) VALUES ('Ecuador');
+INSERT INTO paises(name) VALUES ('El Salvador');
+INSERT INTO paises(name) VALUES ('Guatemala');
+INSERT INTO paises(name) VALUES ('Haití');
+INSERT INTO paises(name) VALUES ('Honduras');
+INSERT INTO paises(name) VALUES ('México');
+INSERT INTO paises(name) VALUES ('Nicaragua');
+INSERT INTO paises(name) VALUES ('Panamá');
+INSERT INTO paises(name) VALUES ('Paraguay');
+INSERT INTO paises(name) VALUES ('Perú');
+INSERT INTO paises(name) VALUES ('Puerto Rico');
+INSERT INTO paises(name) VALUES ('República Dominicana');
+INSERT INTO paises(name) VALUES ('Uruguay');
+INSERT INTO paises(name) VALUES ('Venezuela');
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <?php
+            $estudiante[0] = 2;
+            $estudiante[1] = 4;
+            $estudiante[2] = 1;
+            $estudiante[3] = 5;
+            echo "Numero de hermanos del tercer estudiante:";
+            echo $temp[2], "hermanos";
+        ?>
+    </body>
+</html>
